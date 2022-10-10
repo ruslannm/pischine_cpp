6925f23b66cea51060ba5a5fb5e1111d50701fe4
ruslannm <brak@mail.ru>
2022-10-10 15:06:40 +0300

    day07-ex00

--- pre-day07/ex02/Array.hpp
+++ pre-day07/ex02/Array.hpp
@@ -1,37 +1,43 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
+
 #include <iostream>
 #include <iomanip>
 
 template<typename T>
 class Array
 {
-	public:
-		Array(void);
-		Array(const unsigned int&);
-		Array(const Array<T>&);
-		~Array(void);
+public:
+	Array(void);
 
-		Array<T>&	 operator=(const Array<T>&);
+	Array(const unsigned int &);
 
-		T&	operator[](const int &);
-		T const &	operator[](const int &) const;		
-		unsigned int	size(void) const;
-		class IndexOutOfBonds : public std::exception
+	Array(const Array<T> &);
+
+	~Array(void);
+
+	Array<T> &operator=(const Array<T> &);
+
+	T &operator[](const int &);
+
+	T const &operator[](const int &) const;
+
+	unsigned int size(void) const;
+
+	class IndexOutOfBonds : public std::exception
 	{
 	public:
 		virtual const char *what(void) const throw();
 	};
-		private:
-		T* 		_array;
-		unsigned int 	_size;
+
+private:
+	T *_array;
+	unsigned int _size;
 };
 
 
-
-
 template<typename T>
-std::ostream & operator<<(std::ostream &, const Array<T> &);
+std::ostream &operator<<(std::ostream &, const Array<T> &);
 
 
 #include "Array.tpp"
--- pre-day07/ex02/main.cpp
+++ pre-day07/ex02/main.cpp
@@ -5,49 +5,49 @@
 #define SIZE_2 2
 
 int main(void)
-{	
-     std::cout	<< "Test canonical methods" << std::endl;
-    Array<int> arrDefault;
-    std::cout << "arrDefault " << arrDefault << std::endl;
-  
-    Array<int> arrInt(SIZE);
-    for (unsigned int i = 0; i < SIZE; ++i)
-    {
-        arrInt[i] = i + 1;
+{
+	std::cout << "Test canonical methods" << std::endl;
+	Array<int> arrDefault;
+	std::cout << "arrDefault " << arrDefault << std::endl;
 
-    }
-    std::cout << "arrInt " << arrInt << std::endl;
-    Array<int> arrInt1(arrInt);
-    std::cout << "arrInt1 " << arrInt1 << std::endl;
-    Array<int> arrInt2(SIZE_2);
-    for (unsigned int i = 0; i < SIZE_2; ++i)
-    {
-        arrInt2[i] = i + 100;
-    }
-    std::cout << "arrInt2 " << arrInt2 << std::endl;
-    arrInt2 = arrInt;
-    std::cout << "arrInt2 " << arrInt2 << std::endl;
-    arrInt[0] = 1000;
-std::cout << "arrInt" << arrInt << std::endl;
-std::cout << "arrInt1" << arrInt1 << std::endl;
-std::cout << "arrInt2" << arrInt2 << std::endl;
-std::cout	<< "Test exceptions" << std::endl;
-    try
-    {
-        arrInt[-1] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-	
-    try
-    {
-        arrInt[SIZE] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    return 0;
+	Array<int> arrInt(SIZE);
+	for (unsigned int i = 0; i < SIZE; ++i)
+	{
+		arrInt[i] = i + 1;
+
+	}
+	std::cout << "arrInt " << arrInt << std::endl;
+	Array<int> arrInt1(arrInt);
+	std::cout << "arrInt1 " << arrInt1 << std::endl;
+	Array<int> arrInt2(SIZE_2);
+	for (unsigned int i = 0; i < SIZE_2; ++i)
+	{
+		arrInt2[i] = i + 100;
+	}
+	std::cout << "arrInt2 " << arrInt2 << std::endl;
+	arrInt2 = arrInt;
+	std::cout << "arrInt2 " << arrInt2 << std::endl;
+	arrInt[0] = 1000;
+	std::cout << "arrInt" << arrInt << std::endl;
+	std::cout << "arrInt1" << arrInt1 << std::endl;
+	std::cout << "arrInt2" << arrInt2 << std::endl;
+	std::cout << "Test exceptions" << std::endl;
+	try
+	{
+		arrInt[-1] = 0;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	try
+	{
+		arrInt[SIZE] = 0;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
 }
