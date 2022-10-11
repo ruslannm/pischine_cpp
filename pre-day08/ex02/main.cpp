b10c7c4f69ef875df4214e20eccea219704fcca2
ruslannm <brak@mail.ru>
2022-10-11 11:09:40 +0300

    d08-ex02 add test

--- pre-day08/ex02/main.cpp
+++ pre-day08/ex02/main.cpp
@@ -50,5 +50,42 @@ int main(void)
 			++it;
 		}
 	}
+	{
+		std::cout << "Test Canonical Methods" << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		MutantStack<int> mstack1(mstack);
+		MutantStack<int>::iterator it = mstack1.begin();
+		MutantStack<int>::iterator ite = mstack1.end();
+		std::cout << "mstack1:" << std::endl;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		MutantStack<int> mstack2;
+		mstack2.push(0);
+		mstack2.push(21);
+		mstack2.push(42);
+		mstack2 = mstack;
+		mstack2.push(100);
+		it = mstack2.begin();
+		ite = mstack2.end();
+		std::cout << "mstack2:" << std::endl;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		it = mstack.begin();
+		ite = mstack.end();
+		std::cout << "mstack:" << std::endl;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
 	return 0;
 }
